c7cb30426f4aba0e1f3aeb5d72952ef3f30e6099
ageu_dev19 <ageumenezes423@gmail.com>
2023-12-11 14:51:47 -0300

    Sidebar is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import './global.scss'
+import styles from './App.module.scss';
 import { Header } from './components/Header'
+import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
 
-function App() {
+export  function App() {
 
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Ageu Menezes'
+            content='Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+          />
+          <Post
+            author='Lucas Ferreira'
+            content='Lorem ipsum, adipisicing elit.'
+          />
+        </main>
+      </div>
     </>
   )
 }
-
-export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import styles from './Sidebar.module.scss';
+import { PencilLine } from 'phosphor-react';
+import profileImg from '../assets/IMG_0013.jpeg';
+import backgroundImg from '../assets/photo-1701979397766-bbd6fdd29743.avif';
+
+export function Sidebar() {
+  return(
+    <>
+      <aside className={styles.sidebar}>
+        <img
+          className={styles.cover}
+          src={backgroundImg}
+        />
+        <div className={styles.profile}>
+          <img src={profileImg} />
+          <strong>Ageu Menezes</strong>
+          <span>Web Developer</span>
+
+          <footer>
+            <a href="#">
+              <PencilLine size={20} />
+              Editar seu perfil
+            </a>
+          </footer>
+        </div>
+      </aside>
+    </>
+  )
+}
